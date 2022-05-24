513c4fcfc31290c1368616a0eb60d66324fad36c
Péter Kalicz <kaliczp@gmail.com>
2022-05-24 13:51:00 +0200

    Raw gw data source modified
    The source matrix is manipulated. It is not modified.

--- Beolv.R
+++ Beolv.R
@@ -1,6 +1,5 @@
 ## Read in
 library(readxl)
 nevek <- read_excel("Adat/vegetációs időszakok.xlsx", 1, "A3:C20")
-rawdata <- read_excel("Adat/vegetációs időszakok.xlsx", 1, "H2:HI20")
-
-
+## Table 2 is corrected?
+rawdata <- read_excel("Adat/vegetációs időszakok.xlsx", 2, "G2:IB20")
