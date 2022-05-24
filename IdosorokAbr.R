0246aad0c2e47291e56afb1087d47baa805fb162
Péter Kalicz <kaliczp@gmail.com>
2022-05-24 14:58:45 +0200

    All wells in separate sheets

--- IdosorokAbr.R
+++ IdosorokAbr.R
@@ -1,7 +1,9 @@
 ## Calculate gw limits
 summary(unlist(as.vector(rawdata)))
 
-tti <- 6
-pdf(paste0("gw",tti,".pdf"), width = 420/25.4, height = 297 / 25.4)
-plot.zoo(gw.xts[,tti], main = paste("GW", tti, "(", nevek[1,2], nevek[1,1], ")"), lwd = 2, xaxs = "i", ylim = c(-465, 85))
-dev.off()
+for(tti in 1:ncol(gw.xts)) {
+    pdf(paste0("gw",tti,".pdf"), width = 420/25.4, height = 297 / 25.4)
+    plot.zoo(gw.xts[,tti], main = paste("GW", tti, "(", nevek[tti,2], nevek[tti,1], ")"), lwd = 2, xaxs = "i", ylim = c(-465, 85))
+    dev.off()
+}
+## pdfjam gw* --papersize '{420mm,297mm}' --outfile Gw.pdf
