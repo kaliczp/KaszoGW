49e8bba93dbf769e8fb78867b4757ffdf5569309
Péter Kalicz <kaliczp@gmail.com>
2022-05-24 14:15:49 +0200

    Reverse the scale
    Original data contains reverse scale below the surface is positive measurement.

--- Beolv.R
+++ Beolv.R
@@ -3,6 +3,8 @@ library(readxl)
 nevek <- read_excel("Adat/vegetációs időszakok.xlsx", 1, "A3:C20")
 ## Table 2 is corrected?
 rawdata <- read_excel("Adat/vegetációs időszakok.xlsx", 2, "G2:IB20")
+## Correct the reverse scale
+rawdata <- 0 - rawdata
 
 ### Convert data to timeseries
 ## Transpose (implicitly convert to matrix from tibble)
