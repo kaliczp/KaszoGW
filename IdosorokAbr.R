e849bf3c4a4c3c3397aeef0f7c70d0e4e943db35
Péter Kalicz <kaliczp@gmail.com>
2022-05-24 15:42:08 +0200

    Plott all wells in groups wiht a for loop

--- IdosorokAbr.R
+++ IdosorokAbr.R
@@ -8,42 +8,34 @@ for(tti in 1:ncol(gw.xts)) {
 }
 ## pdfjam gw* --papersize '{420mm,297mm}' --outfile Gw.pdf
 
-## Kontroll kutak
+### Plot data together
 
-pdf(paste0("Controls.pdf"), width = 420/25.4, height = 297 / 25.4)
-tti  <-  2
-ttszin <- 1
-titlepoint <- 14
-plot.zoo(gw.xts[,tti], main = "Kontroll kutak",
-         lwd = 2, xaxs = "i", ylim = c(-465, 85))
-text(x = index(as.zoo(gw.xts[titlepoint ,tti])),
-     y = coredata(as.zoo(gw.xts[titlepoint ,tti])),
-     paste("GW", tti), col = ttszin, lwd = 2, adj = c(0.5, 1))
-for(tti in c(3:4,18)) {
-    ttszin <- ttszin + 1
-    lines(as.zoo(gw.xts[,tti]), col = ttszin)
-    text(x = index(as.zoo(gw.xts[titlepoint ,tti])),
-         y = coredata(as.zoo(gw.xts[titlepoint ,tti])),
-         paste("GW", tti), col = ttszin, lwd = 2, adj = c(0.5, 1.5))
+Separate.df <- data.frame(Name = c("Controls", "KST", "MÉ1", "MÉ2")
+                        , PlotTitle = c("Kontroll kutak", "KST kutak", "Égerek 1", "Égerek 2")
+                        , TimeserNum = c("2,3,4,18",
+                                         "7,8,12,15",
+                                         "1,5,6,9,10",
+                                         "11,13,14,16,17")
+                        , LabelPlace = c(14, 185, 100, 185)
+                          )
+
+for(ttsel in 1:nrow(Separate.df)) {
+    pdf(paste0(Separate.df[ttsel, "Name"], ".pdf"), width = 420/25.4, height = 297 / 25.4)
+    timesernums <- as.numeric(unlist(strsplit(Separate.df[ttsel, "TimeserNum"], ",")))
+    tti  <-  timesernums[1]
+    ttszin <- 1
+    titlepoint <- Separate.df[ttsel, "LabelPlace"]
+    plot.zoo(gw.xts[,tti], main = Separate.df[ttsel, "PlotTitle"],
+             lwd = 2, xaxs = "i", ylim = c(-465, 85), ylab = "GW depth [cm]")
+    text(x = index(as.zoo(gw.xts[titlepoint, tti])),
+         y = coredata(as.zoo(gw.xts[titlepoint, tti])),
+         paste("GW", tti), col = ttszin, lwd = 2, adj = c(0.5, 1))
+    for(tti in timesernums[-1]) {
+        ttszin <- ttszin + 1
+        lines(as.zoo(gw.xts[,tti]), col = ttszin)
+        text(x = index(as.zoo(gw.xts[titlepoint ,tti])),
+             y = coredata(as.zoo(gw.xts[titlepoint ,tti])),
+             paste("GW", tti), col = ttszin, lwd = 2, adj = c(0.5, 1.5))
+    }
+    dev.off()
 }
-dev.off()
-
-
-## KST kutak
-pdf(paste0("KST.pdf"), width = 420/25.4, height = 297 / 25.4)
-tti  <-  7
-ttszin <- 1
-titlepoint <- 185 
-plot.zoo(gw.xts[,tti], main = "KST kutak",
-         lwd = 2, xaxs = "i", ylim = c(-465, 85))
-text(x = index(as.zoo(gw.xts[titlepoint ,tti])),
-     y = coredata(as.zoo(gw.xts[titlepoint ,tti])),
-     paste("GW", tti), col = ttszin, lwd = 2, adj = c(0.5, 1))
-for(tti in c(8,12,15)) {
-    ttszin <- ttszin + 1
-    lines(as.zoo(gw.xts[,tti]), col = ttszin)
-    text(x = index(as.zoo(gw.xts[titlepoint ,tti])),
-         y = coredata(as.zoo(gw.xts[titlepoint ,tti])),
-         paste("GW", tti), col = ttszin, lwd = 2, adj = c(0.5, 1.5))
-}
-dev.off()
