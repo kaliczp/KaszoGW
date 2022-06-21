7dad2f2d3003e1f25720d40bcd57a7264e464cfd
Péter Kalicz <kaliczp@gmail.com>
2022-06-21 15:43:12 +0200

    Table for control and treatment pairing.

---
+++ ContDiff.R
@@ -0,0 +1,8 @@
+## Space-time difference
+
+Diff.df <- data.frame(
+    Treat = c(5,6,7,8,9,10,11,12,13,14,15,16,17,1),
+    Control = c(4,18,2,3,18,18,18,3,18,18,3,18,18,18),
+    Tree = factor(c("A", "A", "O", "O", "A", "A", "A", "O", "A", "A", "O", "A", "A", "A"))
+)
+
