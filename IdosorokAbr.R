9b050d3b1a2f1f70b964e45d51b76957a89898b6
Péter Kalicz <kaliczp@gmail.com>
2022-09-15 16:33:59 +0200

    Time-series shrinked to publication size

--- IdosorokAbr.R
+++ IdosorokAbr.R
@@ -19,6 +19,7 @@ Separate.df <- data.frame(Name = c("Controls", "KST", "MÉ1", "MÉ2")
                         , LabelPlace = c(14, 185, 100, 185)
                           )
 
+## For A3 page
 for(ttsel in 1:nrow(Separate.df)) {
     pdf(paste0(Separate.df[ttsel, "Name"], ".pdf"), width = 420/25.4, height = 297 / 25.4)
     timesernums <- as.numeric(unlist(strsplit(Separate.df[ttsel, "TimeserNum"], ",")))
@@ -33,6 +34,29 @@ for(ttsel in 1:nrow(Separate.df)) {
     for(tti in timesernums[-1]) {
         ttszin <- ttszin + 1
         lines(as.zoo(gw.xts[,tti]), col = ttszin)
+        text(x = index(as.zoo(gw.xts[titlepoint ,tti])),
+             y = coredata(as.zoo(gw.xts[titlepoint ,tti])),
+             paste("GW", tti), col = ttszin, lwd = 2, adj = c(0.5, 1.5))
+    }
+    dev.off()
+}
+
+## For publication
+for(ttsel in 1:nrow(Separate.df)) {
+    pdf(paste0(Separate.df[ttsel, "Name"], ".pdf"), width = 180/25.4, height = 50 / 25.4)
+    timesernums <- as.numeric(unlist(strsplit(Separate.df[ttsel, "TimeserNum"], ",")))
+    tti  <-  timesernums[1]
+    ttszin <- 1
+    titlepoint <- Separate.df[ttsel, "LabelPlace"]
+    par(mar = c(2.1, 4.1, 0.3, 0.1), las = 1)
+    plot.zoo(gw.xts[,tti], main = "",
+             lwd = 2, xaxs = "i", ylim = c(-465, 85), ylab = "GW depth [cm]")
+    text(x = index(as.zoo(gw.xts[titlepoint, tti])),
+         y = coredata(as.zoo(gw.xts[titlepoint, tti])),
+         paste("GW", tti), col = ttszin, lwd = 2, adj = c(0.5, 1))
+    for(tti in timesernums[-1]) {
+        ttszin <- ttszin + 1
+        lines(as.zoo(gw.xts[,tti]), col = ttszin, lwd = 2)
         text(x = index(as.zoo(gw.xts[titlepoint ,tti])),
              y = coredata(as.zoo(gw.xts[titlepoint ,tti])),
              paste("GW", tti), col = ttszin, lwd = 2, adj = c(0.5, 1.5))
