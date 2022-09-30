94437f642b77103ff33571c6ca8932f9af04ec2a
Péter Kalicz <kaliczp@gmail.com>
2022-09-30 17:31:38 +0200

    Table created for all t-tests with p-value and means

--- Tablettest.R
+++ Tablettest.R
@@ -4,5 +4,16 @@ NoShftFull.df$Mode <- factor(NoShftFull.df$Mode, levels = c("B", "A"), labels = 
 ## Treatment wells
 Diff.df$Treat
 
-treatwellnum <- 1
-t.test(Diff ~ Mode, NoShftFull.df[NoShftFull.df$WellT == Diff.df[treatwellnum, "Treat"], ])
+ForTable.df <- Diff.df
+ForTable.df$meanBfr  <-  numeric(14)
+ForTable.df$meanAftr  <-  numeric(14)
+ForTable.df$p.value  <-  numeric(14)
+
+for(treatwellnum in 1:nrow(Diff.df)) {
+takt <- t.test(Diff ~ Mode, NoShftFull.df[NoShftFull.df$WellT == Diff.df[treatwellnum, "Treat"], ])
+ForTable.df[treatwellnum, "meanBfr"] <- takt$estimate[1]
+ForTable.df[treatwellnum, "meanAftr"] <- takt$estimate[2]
+ForTable.df[treatwellnum, "p.value"] <- takt$p.value
+}
+
+write.table(ForTable.df, file = "ForTable.csv")
