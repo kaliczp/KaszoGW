d7e5985b26d5819dea5066560349ce5e782673ff
Péter Kalicz <kaliczp@gmail.com>
2022-09-30 15:59:39 +0200

    Longer label for tree species

--- ContDiff.R
+++ ContDiff.R
@@ -3,7 +3,7 @@
 Diff.df <- data.frame(
     Treat = c(5,6,7,8,9,10,11,12,13,14,15,16,17,1),
     Control = c(4,18,2,3,18,18,18,3,18,18,3,18,18,18),
-    Tree = factor(c("A", "A", "O", "O", "A", "A", "A", "O", "A", "A", "O", "A", "A", "A")),
+    Tree = factor(c("A", "A", "O", "O", "A", "A", "A", "O", "A", "A", "O", "A", "A", "A"), labels = c("Alder", "Oak")),
     TreatmentType = factor(c("Weir", "Weir", "Reservoir", "Reservoir", "Reservoir", "Weir",
                              "Weir", "Weir", "Weir", "Weir", "Weir", "Weir", "Weir", "Weir"))
 )
