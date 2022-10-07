3409af8dc3a76397462e3d3e6318d5c3680a52ea
Péter Kalicz <kaliczp@gmail.com>
2022-10-07 10:50:50 +0200

    Talajvíz szint a felszíntől

--- Groundwaterlong.R
+++ Groundwaterlong.R
@@ -5,12 +5,12 @@ GWnyers.df <- as.data.frame(read_excel("Adat/Nyers észlelt_1998_2004.xlsx", ran
 
 ## Conversion to xts
 library(xts)
-GWfeld.xts <- xts(160.41 - GWfeld.df[,2]/100, GWfeld.df[,"Idopont"])
-GWnyers.xts <- xts(160.41 - GWnyers.df[,2]/100, GWnyers.df[,"Idopont"])
+GWfeld.xts <- xts(1 - GWfeld.df[,2]/100, GWfeld.df[,"Idopont"])
+GWnyers.xts <- xts(1 - GWnyers.df[,2]/100, GWnyers.df[,"Idopont"])
 
 
 ## Plot
 par(las = 1, mar = c(3.1, 4.6, 0.2, 0.2), mgp = c(3.5,1,0))
-plot.zoo(c(GWnyers.xts,GWfeld.xts), xlab = "", ylab = "GW level [a.s.l. m]", xaxs = "i", type = "n")
+plot.zoo(c(GWnyers.xts,GWfeld.xts), xlab = "", ylab = "GW level [from surface m]", xaxs = "i", type = "n")
 grid(nx = NA, ny = NULL)
 lines(as.zoo(c(GWnyers.xts,GWfeld.xts)), lwd = 2)
