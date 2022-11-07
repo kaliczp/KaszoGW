c3849b99853f4cdb7d70af2a3bd35e0c72e48ac5
Péter Kalicz <kaliczp@gmail.com>
2022-11-07 21:30:17 +0100

    Plot longterm gw ataset into pdf file

--- Groundwaterlong.R
+++ Groundwaterlong.R
@@ -10,7 +10,9 @@ GWnyers.xts <- xts(1 - GWnyers.df[,2]/100, GWnyers.df[,"Idopont"])
 
 
 ## Plot
-par(las = 1, mar = c(3.1, 4.6, 0.2, 0.2), mgp = c(3.5,1,0))
+pdf("/HA20221104/LongTerm.pdf", height = 12 / 2.54, width = 18 / 2.54, points = 11)
+par(las = 1, mar = c(3.1, 4.6, 0.1, 0.1), mgp = c(3.5,1,0))
 plot.zoo(c(GWnyers.xts,GWfeld.xts), xlab = "", ylab = "GW level [from surface m]", xaxs = "i", type = "n")
 grid(nx = NA, ny = NULL)
 lines(as.zoo(c(GWnyers.xts,GWfeld.xts)), lwd = 2)
+dev.off()
