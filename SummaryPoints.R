5bfb6857f7f09cfac664d7cae9000d649d914ba6
Péter Kalicz <kaliczp@gmail.com>
2023-02-13 05:53:48 +0100

    Initial summary of differences plot

---
+++ SummaryPoints.R
@@ -0,0 +1,23 @@
+## Pontdiagram a különbségekre
+
+SummaryPoints.df <- data.frame(
+    Median = numeric(),
+    Diff = numeric(),
+    WellC = numeric(),
+    WellT = numeric()
+)
+for(tti in 1:nrow(Diff.df)) {
+    aktNumCtrl <- Diff.df[tti, "Control"]
+    aktNumTreat <- Diff.df[tti, "Treat"]
+    akt.num <- paste0(aktNumCtrl, aktNumTreat)
+    akt.median <- median(gw.xts[, aktNumTreat])
+    akt.difference <- median(NoShftFull.df[NoShftFull.df$Code == paste0("B", akt.num), "Diff"]) -
+        median(NoShftFull.df[NoShftFull.df$Code == paste0("A", akt.num), "Diff"])
+    akt.df <- data.frame(Median = akt.median,
+                         Diff = akt.difference,
+                         WellC = aktNumCtrl,
+                         WellT = aktNumTreat
+                         )
+    SummaryPoints.df <- rbind(SummaryPoints.df, akt.df)
+}
+plot(Diff ~ Median, SummaryPoints.df)
