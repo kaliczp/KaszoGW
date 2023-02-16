71b3f7b7469fc0bb13d8f112a71030ecc08547d0
Péter Kalicz <kaliczp@gmail.com>
2023-02-16 22:47:27 +0100

    X axis is reversed and A - B instead B - A

--- SummaryPoints.R
+++ SummaryPoints.R
@@ -13,8 +13,8 @@ for(tti in 1:nrow(Diff.df)) {
     aktNumTreat <- Diff.df[tti, "Treat"]
     akt.num <- paste0(aktNumCtrl, aktNumTreat)
     akt.median <- median(gw.xts[, aktNumTreat])
-    akt.difference <- median(NoShftFull.df[NoShftFull.df$Code == paste0("B", akt.num), "Diff"]) -
-        median(NoShftFull.df[NoShftFull.df$Code == paste0("A", akt.num), "Diff"])
+    akt.difference <- median(NoShftFull.df[NoShftFull.df$Code == paste0("A", akt.num), "Diff"]) -
+        median(NoShftFull.df[NoShftFull.df$Code == paste0("B", akt.num), "Diff"])
     akt.df <- data.frame(Median = akt.median,
                          Diff = akt.difference,
                          Tree = Diff.df[tti, "Tree"],
@@ -24,6 +24,7 @@ for(tti in 1:nrow(Diff.df)) {
                          )
     SummaryPoints.df <- rbind(SummaryPoints.df, akt.df)
 }
+SummaryPoints.df$Median <- SummaryPoints.df$Median * -1
 
 pdf("SummaryPoints.pdf", width = 10)
 plot(Diff ~ Median, SummaryPoints.df, type = "n")
