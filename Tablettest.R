16b407d9bf0201213c30d1b4b9438be717d8e883
Péter Kalicz <kaliczp@gmail.com>
2023-02-28 05:36:46 +0100

    Statistical table for vegetation period only

--- Tablettest.R
+++ Tablettest.R
@@ -17,3 +17,19 @@ ForTable.df[treatwellnum, "p.value"] <- round(takt$p.value, 3)
 }
 
 write.table(ForTable.df, file = "ForTable.csv", dec = ",", row.names = FALSE)
+
+## Vegetation period
+ForTableVeget.df <- Diff.df
+ForTableVeget.df$meanBfr  <-  numeric(14)
+ForTableVeget.df$meanAftr  <-  numeric(14)
+ForTableVeget.df$p.value  <-  numeric(14)
+
+for(treatwellnum in 1:nrow(Diff.df)) {
+    takt <- t.test(Diff ~ Mode, NoShftFull.df[NoShftFull.df$WellT == Diff.df[treatwellnum, "Treat"] &
+                                            NoShftFull.df$Season == "Veget", ])
+    ForTableVeget.df[treatwellnum, "meanBfr"] <- round(takt$estimate[1], 2)
+    ForTableVeget.df[treatwellnum, "meanAftr"] <- round(takt$estimate[2], 2)
+    ForTableVeget.df[treatwellnum, "p.value"] <- round(takt$p.value, 3)
+}
+
+write.table(ForTableVeget.df, file = "ForTableVeget.csv", dec = ",", row.names = FALSE)
