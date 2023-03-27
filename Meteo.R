b9e24213f8df5242f5542bae2a311cb3d39e072f
Péter Kalicz <kaliczp@gmail.com>
2023-03-27 15:48:54 +0200

    Meteo data based on monthly precipitation
    Precipitation sums are calculated for months and shifted to look better.
    
    Daily figure tailored for monthly precipitation. Axes, limits and labels are moved.

--- Meteo.R
+++ Meteo.R
@@ -32,5 +32,34 @@ dev.off()
 
 ## Yearly and monthly sums
 csapyear.xts <- apply.yearly(csap.xts, function(x){sum(x, na.rm = TRUE)})
-csapmonth.xts <- apply.monthly(csap.xts, function(x){sum(x, na.rm = TRUE)})
+csapmonthnoshift.xts <- apply.monthly(csap.xts, function(x){sum(x, na.rm = TRUE)})
+csapmonth.xts <- xts(coredata(csapmonthnoshift.xts), index(csapmonthnoshift.xts) - 15 * 24 * 60 *60)
 write.zoo(csapmonth.xts, "csapmonth.csv", dec = ",", sep = ";", eol = "\r\n", quote = FALSE)
+
+pdf("meteomonth.pdf", width = 18/2.54, height = 8/2.54)
+par(mar = c(2.1, 3.4, 0.5, 4.7), las = 1, lend = 1)
+plot.zoo(csapmonth.xts, type = "n",
+         xaxs = "i", yaxs = "i",
+         xlab = "", ylab = "",
+         xlim = IdoLim, ylim = c(510, 0),
+         yaxt = "n") # xaxt = "n", 
+grid(nx = NA, ny = NULL)
+axis(4, at = c(50, 150), tck = 1, col = "lightgray", lty = "dotted")
+axis.POSIXct(1, as.POSIXct(paste(2015:2018, "01-01", sep = "-")),
+     tck = 1, lab = FALSE, col = "lightgray", lty = "dotted")
+# axis(4)
+axis(4, c(0,50,100,150))
+mtext("Precipitation", adj = 0.5, at = 75, side = 4, line = 2.5, las = 0, col = "blue")
+mtext("[mm/month]", adj = 0.5, at = 75, side = 4, line = 3.5, las = 0, col = "blue")
+lines(as.zoo(csapmonth.xts), type = "h", col = "blue", lwd = 5)
+par(new = TRUE, lend = 0)
+plot.zoo(hom.xts, type ="n",
+         xaxs = "i", yaxs = "i",
+         xlab = "", ylab = "",
+         axes = FALSE,
+         xlim = IdoLim, ylim = c(-11, 40))
+lines(as.zoo(hom.xts), lwd = 2)
+axis(2, c(-10,0,10,20,30))
+mtext(expression(paste("Daily temperature [",degree*C,"]")), side = 2, at = 10, adj = 0.5, line = 2.1, las = 0)
+box()
+dev.off()
