b2e3fb057d488e32f857b5a062d95b7a3ed477c7
Kalicz Péter <kaliczp@gmail.com>
2024-06-05 16:20:48 +0200

    Initial figure

--- SpaceTimeFig.R
+++ SpaceTimeFig.R
@@ -4,3 +4,6 @@ library(readxl)
 RawTable <- as.data.frame(read_excel("Teszt.xlsx"))
 GrowingTable <- RawTable[3:16,1:10]
 FullTable <- RawTable[21:34,1:10]
+
+plot(1:nrow(GrowingTable), GrowingTable[,10])
+points(1:nrow(FullTable), FullTable[,10])
