c8eea4736d3cf4a911f756f04272c914fa89173f
Kalicz Péter <kaliczp@gmail.com>
2024-06-20 18:01:03 +0200

    Meteo axes titles are changed

--- Meteo.R
+++ Meteo.R
@@ -19,12 +19,12 @@ axis(1, as.POSIXct(paste(2015:2018, "01-01", sep = "-")),
      tck = 1, lab = FALSE, col = "lightgray", lty = "dotted")
 axis(4)
 axis(4, c(10,30,50,70), lab = FALSE)
-mtext("Csapadék [mm]", side = 4, line = 2.5, las = 0, col = "blue")
+mtext("Napi csapadékösszeg [mm]", side = 4, line = 2.5, las = 0, col = "blue")
 lines(as.zoo(csap.xts), type = "h", col = "blue", lwd = 2)
 par(new = TRUE, lend = 0)
 plot.zoo(hom.xts, type ="n",
          xaxs = "i", yaxs = "i",
-         xlab = "", ylab = expression(paste("Hőmérséklet [",degree*C,"]")),
+         xlab = "", ylab = expression(paste("Napi hőmérséklet [",degree*C,"]")),
          xlim = IdoLim, ylim = c(-11, 35))
 lines(as.zoo(hom.xts), lwd = 2)
 box()
