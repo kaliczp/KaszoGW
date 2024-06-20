91a613062dd55d652573f67cb8055a30da867079
Kalicz Péter <kaliczp@gmail.com>
2024-06-20 18:11:43 +0200

    Resize figure

--- Meteo.R
+++ Meteo.R
@@ -7,8 +7,8 @@ hom.xts <- xts(dplyr::pull(raw, 2), dplyr::pull(raw,Date))
 ## Ábrázolás
 IdoLim <- c(as.POSIXct("2014-10-01"), as.POSIXct("2018-10-16"))
 
-pdf("meteo.pdf", width = 18/2.54, height = 8/2.54)
-par(mar = c(2.1, 4.1, 0.5, 4.1), las = 1, lend = 1)
+pdf("meteo.pdf", width = 12.5/2.54, height = 7/2.54, pointsize = 10)
+par(mar = c(2.1, 3.1, 0.5, 3.1), las = 1, lend = 1)
 plot.zoo(csap.xts, type = "n",
          xaxs = "i", yaxs = "i",
          xlab = "", ylab = "",
@@ -19,9 +19,9 @@ axis(1, as.POSIXct(paste(2015:2018, "01-01", sep = "-")),
      tck = 1, lab = FALSE, col = "lightgray", lty = "dotted")
 axis(4)
 axis(4, c(10,30,50,70), lab = FALSE)
-mtext("Napi csapadékösszeg [mm]", side = 4, line = 2.5, las = 0, col = "blue")
+mtext("Napi csapadékösszeg [mm]", side = 4, line = 2, las = 0, col = "blue")
 lines(as.zoo(csap.xts), type = "h", col = "blue", lwd = 2)
-par(new = TRUE, lend = 0)
+par(new = TRUE, lend = 0, mgp = c(2,1,0))
 plot.zoo(hom.xts, type ="n",
          xaxs = "i", yaxs = "i",
          xlab = "", ylab = expression(paste("Napi hőmérséklet [",degree*C,"]")),
